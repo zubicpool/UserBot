5df377301e19c4ac3b2f2d885e9304c9f028d8af
shivamkchoudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-06-09 21:23:16 +0530

    Create spammer.py

---
+++ userbot/modules/spammer.py
@@ -0,0 +1,96 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.b (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+import asyncio
+from asyncio import wait
+
+from userbot import LOGGER_GROUP, LOGGER, HELPER
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.tspam")
+async def tmeme(e):
+    tspam = str(e.text[7:])
+    message = tspam.replace(" ", "")
+    for letter in message:
+        await e.respond(letter)
+    await e.delete()
+
+@register(outgoing=True, pattern="^.spam")
+async def spammer(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[6:8])
+        spam_message = str(e.text[8:])
+        await asyncio.wait([e.respond(spam_message) for i in range(counter)])
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP,
+                "#SPAM \n\n"
+                "Spam was executed successfully"
+                )
+                               
+@register(outgoing=True, pattern="^.bigspam")
+async def bigspam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[9:13])
+        spam_message = str(e.text[13:])
+        for i in range(1, counter):
+            await e.respond(spam_message)
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP,
+                "#BIGSPAM \n\n"
+                "Bigspam was executed successfully"
+                )
+        
+        
+@register(outgoing=True, pattern="^.gangsta$")
+async def whoizme(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("EVERyBOdy")
+        await asyncio.sleep(0.3)
+        await e.edit("iZ")
+        await asyncio.sleep(0.2)
+        await e.edit("GangSTur")
+        await asyncio.sleep(0.5)
+        await e.edit("UNtIL ")
+        await asyncio.sleep(0.2)
+        await e.edit("I")
+        await asyncio.sleep(0.3)
+        await e.edit("ArRivE")
+        await asyncio.sleep(0.3)
+        await e.edit("🔥")
+        await asyncio.sleep(0.3)
+        await e.edit("EVERyBOdy iZ GangSTur UNtIL I ArRivE 🔥")
+
+@register(outgoing=True, pattern="^.repeat")
+async def repeat(e):
+    message = e.text[10:]
+    count = int(e.text[8:10])
+    repmessage = message * count
+    await e.respond(repmessage)
+    await e.delete()
+    
+    @register(outgoing=True, pattern="^.picspam")
+async def tiny_pic_spam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        text = message.split()
+        counter = int(text[1])
+        link = str(text[2])
+        for i in range(1, counter):
+            await e.client.send_file(e.chat_id, link)
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP,
+                "#PICSPAM \n\n"
+                "PicSpam was executed successfully"
+                )
+    
