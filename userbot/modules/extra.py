f6c417391c09f65249e4324f84ff427e138b03ec
shivamkchoudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-06-09 23:35:50 +0530

    Update extra.py

--- userbot/modules/extra.py
+++ userbot/modules/extra.py
@@ -64,12 +64,12 @@ async def _(event):
 @register(outgoing=True, pattern="^.sauce$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("https://github.com/Yasir-siddiqui/Userbot/")
+        await e.edit("https://github.com/shivamkchoudhary/Userbot/")
 
 @register(outgoing=True, pattern="^.readme$")
 async def reedme(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("https://github.com/Yasir-siddiqui/UserBot/blob/master/README.md")
+        await e.edit("https://github.com/shivamkchoudhary/UserBot/blob/master/README.md")
 
 @register(outgoing=True, pattern="^.disapprove$")
 async def disapprovepm(disapprvpm):
