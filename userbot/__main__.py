ba05740bf683c74110c88c79501c37fbe5313884
shivamkchoudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-06-10 10:58:50 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -10,20 +10,12 @@ from sqlite3 import connect
 from sys import argv
 
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BRAIN_CHECKER, LOGS, bot
+from userbot import LOGS, bot
 from userbot.modules import ALL_MODULES
 
-DB = connect("learning-data-root.check")
-CURSOR = DB.cursor()
-CURSOR.execute("""SELECT * FROM BRAIN1""")
-ALL_ROWS = CURSOR.fetchall()
 INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
              '\n  Tip: Use Country Code along with No.' \
              '\n       Recheck your Phone Number'
-
-for i in ALL_ROWS:
-    BRAIN_CHECKER.append(i[0])
-connect("learning-data-root.check").close()
 try:
     bot.start()
 except PhoneNumberInvalidError:
@@ -35,7 +27,7 @@ for module_name in ALL_MODULES:
 
 LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
           " Should you need assistance, head to https://t.me/userbot_support")
-LOGS.info("Your Bot Version is 3.0")
+LOGS.info("Your Bot Version is 2.4.2")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
