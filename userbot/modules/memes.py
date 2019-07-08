dee1758c8f941f204ed64c60601444298d76785d
zubicpool <52673039+zubicpool@users.noreply.github.com>
2019-07-09 01:04:07 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -406,7 +406,7 @@ async def hacking (hacked):
         if await hacked.get_reply_message():
             await hacked.edit(
                 "`Targeted Account Hacked successfully 😎......`\n"
-                "`Pay 6969$ to` @shivamkchoudhary `To Remove This Hack...`\n"
+                "`Pay 6969$ to` @zubicpool `To Remove This Hack...`\n"
             )
 
 @register(outgoing=True, pattern="^.kill$")
